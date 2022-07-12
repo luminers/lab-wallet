bec7f6079b5f3ee6d27e2ea96e205102d7aae268
Joaquin nunez <magnanimoysimple@gmail.com>
2022-07-12 14:44:45 -0300

    First Commit

---
+++ src/components/Login.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="login-container">
+    <div class="login">
+      <input id="input-login" type="text" placeholder="Id" v-model="this.Id" />
+      <input id="btn-login" type="button" @click="saveId" value="Ingresar" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginComponent",
+  data() {
+    return {
+      Id: "",
+    };
+  },
+  props: {},
+  methods: {
+    saveId() {
+      this.$store.state.Id = this.Id;
+      console.log(this.$store.state.Id);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.login-container {
+  background: #f5eddc;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#btn-login {
+  width: 50%;
+  align-self: center;
+  margin-top: 20px;
+  background: #eb1d36;
+  border: none;
+  border-radius: 5px;
+  padding: 5px;
+}
+#input-login {
+  background: #a2b5bb;
+  border: 1px solid #eb1d36;
+  border-radius: 5px;
+  height: 2em;
+}
+
+.login {
+  background: #cfd2cf;
+  border: 1px solid #a2b5bb;
+  border-radius: 5px;
+  padding: 30px;
+  display: flex;
+  flex-flow: column wrap;
+}
+#btn-login:hover {
+  background: #ff001e;
+  cursor: pointer;
+}
+</style>
